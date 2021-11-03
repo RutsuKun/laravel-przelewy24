c743c35115618730dc512de6a25d5eb244128a6c
UniTreeDev <user@user.com>
2021-11-03 11:42:22 +0000

    UniTreeDev-user@user.com at: 2021-11-03 11:42:22 UTC

--- tests/Services/AmountTest.php
+++ tests/Services/AmountTest.php
@@ -10,14 +10,15 @@ class CountryTest extends UnitTestCase
     public function provideAmount()
     {
         return [
-            '0' => ['amount' => 123, 'expected' => 12300],
-            '1' => ['amount' => 123.45, 'expected' => 12345],
-            '2' => ['amount' => '123.45', 'expected' => 12345],
             '3' => ['amount' => '123,45', 'expected' => 12345],
             '4' => ['amount' => 'asdf', 'expected' => 0],
             '5' => ['amount' => '0,01', 'expected' => 1],
             '6' => ['amount' => 1, 'expected' => 100],
-            '7' => ['amount' => 001.11, 'expected' => 111],
+            '7' => ['amount' => 1.11, 'expected' => 111],
+            '0' => ['amount' => 123, 'expected' => 12300],
+            '1' => ['amount' => 123.45, 'expected' => 12345],
+            '2' => ['amount' => '123.45', 'expected' => 12345],
+            '-1' => ['amount' => 99, 'expected' => 9900],
         ];
     }
 
