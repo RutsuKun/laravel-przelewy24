6e3803b4d4f07148b2c2ad77eb593634a1ae9778
kamil.p <kamil.p@devpark.pl>
2021-11-08 12:06:37 +0100

    add Api suite

--- tests/Services/Amount/AmountTest.php
+++ tests/Services/Amount/AmountTest.php
@@ -1,24 +1,13 @@
 <?php
 
-namespace Tests\Services;
+namespace Tests\Services\Amount;
 
 use Devpark\Transfers24\Services\Amount;
 use Tests\UnitTestCase;
 
 class AmountTest extends UnitTestCase
 {
-    public function provideAmount()
-    {
-        return [
-            '7' => ['amount' => 1.11, 'expected' => 111],
-            '1' => ['amount' => 123.45, 'expected' => 12345],
-            '2' => ['amount' => '123.45', 'expected' => 12345],
-            '3' => ['amount' => '123,45', 'expected' => 12345],
-            '4' => ['amount' => 'asdf', 'expected' => 0],
-            '5' => ['amount' => '0,01', 'expected' => 1],
-            '6' => ['amount' => 1, 'expected' => 100],
-        ];
-    }
+    use AmountTrait;
 
     protected function setUp()
     {
