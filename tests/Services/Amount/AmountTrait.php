6fd110510da58142fe9263aaef86f8c7e51b8835
kamil.p <kamil.p@devpark.pl>
2021-11-09 09:47:00 +0100

    refactor test trait

--- tests/Services/Amount/AmountTrait.php
+++ tests/Services/Amount/AmountTrait.php
@@ -8,15 +8,15 @@ trait AmountTrait
     public function provideAmount()
     {
         return [
-            '7' => ['amount' => 1.11, 'expected' => 111],
-            '1' => ['amount' => 123.45, 'expected' => 12345],
-            '2' => ['amount' => '123.45', 'expected' => 12345],
-            '3' => ['amount' => '123,45', 'expected' => 12345],
-            '4' => ['amount' => 'asdf', 'expected' => 0],
-            '5' => ['amount' => '0,01', 'expected' => 1],
-            '6' => ['amount' => 1, 'expected' => 100],
-            '9999' => ['amount' => 99.99, 'expected' => 9999],
-            '9' => ['amount' => 0.99, 'expected' => 99],
+            ['amount' => 1.11, 'expected' => 111],
+            ['amount' => 123.45, 'expected' => 12345],
+            ['amount' => '123.45', 'expected' => 12345],
+            ['amount' => '123,45', 'expected' => 12345],
+            ['amount' => 'asdf', 'expected' => 0],
+            ['amount' => '0,01', 'expected' => 1],
+            ['amount' => 1, 'expected' => 100],
+            ['amount' => 99.99, 'expected' => 9999],
+            ['amount' => 0.99, 'expected' => 99],
         ];
     }
 }
