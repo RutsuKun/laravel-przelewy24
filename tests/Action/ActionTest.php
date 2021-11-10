1402a2f9206dace8f4c5f25c111ccbcaa4f71aee
UniTreeDev <user@user.com>
2021-11-10 10:34:03 +0000

    UniTreeDev-user@user.com at: 2021-11-10 10:34:03 UTC

--- tests/Action/ActionTest.php
+++ tests/Action/ActionTest.php
@@ -85,19 +85,18 @@ class ActionTest extends UnitTestCase
      * @feature Payment Methods
      * @scenario Get Payment Methods
      * @case Does send Request Action executed
-     *
+     * 
      * @feature Refunds
      * @scenario Init Refund
      * @case Does send Request Action executed
-     *
+     * 
      * @feature Online Payments
      * @scenario Register Payment
      * @case Does send Request Action executed
-     *
+     * 
      * @feature Connection With Provider
      * @scenario Test Connection
-     * @case Does send Request Action executed
-     *
+     * 
      * @test
      */
     public function execute()
