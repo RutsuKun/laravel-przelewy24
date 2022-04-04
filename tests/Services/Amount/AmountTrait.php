4b9c0b7af65b185443bb98f3639d5b4acc00ac3b
RutsuKun <henicz19@gmail.com>
2022-04-04 10:23:43 +0200

    fix data provider

--- tests/Services/Amount/AmountTrait.php
+++ tests/Services/Amount/AmountTrait.php
@@ -8,17 +8,17 @@ trait AmountTrait
     public function provideAmount()
     {
         return [
-            '0' => ['amount' => 1.11, 'expected' => 111], // test passed
-            '1' => ['amount' => 123.45, 'expected' => 12345], // test passed
-            '2' => ['amount' => '123.45', 'expected' => 12345], // test passed
-            '3' => ['amount' => '123,45', 'expected' => 12345], // test passed
-            '4' => ['amount' => 'asdf', 'expected' => 0], // test passed
-            '5' => ['amount' => '0,01', 'expected' => 1], // test passed
-            '6' => ['amount' => 1, 'expected' => 100], // test passed
-            '7' => ['amount' => 99.99, 'expected' => 9999], // test passed
-            '8' => ['amount' => 0.99, 'expected' => 99], // test passed
-            '9' => ['amount' => 999.99, 'expected' => 99999], // test passed
-            '10' => ['amount' => 'asd', 'expected' => 1], // test failed
+            '0' => ['amount' => 1.11, 'expected' => 111],
+            '1' => ['amount' => 123.45, 'expected' => 12345],
+            '2' => ['amount' => '123.45', 'expected' => 12345],
+            '3' => ['amount' => '123,45', 'expected' => 12345],
+            '4' => ['amount' => 'asdf', 'expected' => 0],
+            '5' => ['amount' => '0,01', 'expected' => 1],
+            '6' => ['amount' => 1, 'expected' => 100],
+            '7' => ['amount' => 99.99, 'expected' => 9999],
+            '8' => ['amount' => 0.99, 'expected' => 99],
+            '9' => ['amount' => 999.99, 'expected' => 99999],
+            '10' => ['amount' => 'asd', 'expected' => 1],
         ];
     }
 }
